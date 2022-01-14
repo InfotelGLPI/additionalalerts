20ff435f563119d6d5615b24fad529625a146b8a
tsmr <thetsmr@gmail.com>
2022-01-14 09:17:16 +0100

    Update headers & int unsigned

--- sql/empty-2.4.0.sql
+++ sql/empty-2.4.0.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_configs`;
 CREATE TABLE `glpi_plugin_additionalalerts_configs` (
-   `id` int(11) NOT NULL auto_increment,
-   `use_infocom_alert` TINYINT( 1 ) NOT NULL DEFAULT '-1',
-   `use_ink_alert` TINYINT( 1 ) NOT NULL DEFAULT '-1',
-   `delay_ticket_alert` int(11) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `use_infocom_alert` tinyint NOT NULL DEFAULT '-1',
+   `use_ink_alert` tinyint NOT NULL DEFAULT '-1',
+   `delay_ticket_alert` int unsigned NOT NULL default '0',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
@@ -12,47 +12,47 @@ VALUES ('1','-1','-1','-1');
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_infocomalerts`;
 CREATE TABLE `glpi_plugin_additionalalerts_infocomalerts` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
-   `use_infocom_alert` TINYINT( 1 ) NOT NULL DEFAULT '-1',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
+   `use_infocom_alert` tinyint NOT NULL DEFAULT '-1',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_ticketunresolveds`;
 CREATE TABLE `glpi_plugin_additionalalerts_ticketunresolveds` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
-   `delay_ticket_alert` int(11) NOT NULL DEFAULT '-1',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
+   `delay_ticket_alert` int unsigned NOT NULL DEFAULT '-1',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_inkalerts`;
 CREATE TABLE `glpi_plugin_additionalalerts_inkalerts` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
-   `use_ink_alert` TINYINT( 1 ) NOT NULL DEFAULT '-1',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
+   `use_ink_alert` tinyint NOT NULL DEFAULT '-1',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_notificationtypes`;
 CREATE TABLE `glpi_plugin_additionalalerts_notificationtypes` (
    `id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
-   `types_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_computertypes (id)',
+   `types_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_computertypes (id)',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_inkthresholds`;
 CREATE TABLE `glpi_plugin_additionalalerts_inkthresholds` (
-   `id` int(11) NOT NULL AUTO_INCREMENT,
-   `cartridges_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_cartridgeitems (id)',
-   `threshold` int(3) NOT NULL default '10',
+   `id` int unsigned NOT NULL AUTO_INCREMENT,
+   `cartridges_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_cartridgeitems (id)',
+   `threshold` int unsigned NOT NULL default '10',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_inkprinterstates`;
 CREATE TABLE `glpi_plugin_additionalalerts_inkprinterstates` (
    `id` INT( 11 ) NOT NULL AUTO_INCREMENT,
-   `states_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_states (id)',
+   `states_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_states (id)',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
