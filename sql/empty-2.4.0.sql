8b671843db0c540396724bd8778088729c051c31
tsmr <thetsmr@gmail.com>
2022-01-19 18:45:21 +0100

    Fix install

--- sql/empty-2.4.0.sql
+++ sql/empty-2.4.0.sql
@@ -1,20 +1,20 @@
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_configs`;
 CREATE TABLE `glpi_plugin_additionalalerts_configs` (
    `id` int unsigned NOT NULL auto_increment,
-   `use_infocom_alert` tinyint NOT NULL DEFAULT '-1',
-   `use_ink_alert` tinyint NOT NULL DEFAULT '-1',
+   `use_infocom_alert` tinyint NOT NULL DEFAULT '0',
+   `use_ink_alert` tinyint NOT NULL DEFAULT '0',
    `delay_ticket_alert` int unsigned NOT NULL default '0',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 INSERT INTO `glpi_plugin_additionalalerts_configs` ( `id`, `use_infocom_alert`, `use_ink_alert`, `delay_ticket_alert`)
-VALUES ('1','-1','-1','-1');
+VALUES ('1','0','0','0');
 
 DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_infocomalerts`;
 CREATE TABLE `glpi_plugin_additionalalerts_infocomalerts` (
    `id` int unsigned NOT NULL auto_increment,
    `entities_id` int unsigned NOT NULL default '0',
-   `use_infocom_alert` tinyint NOT NULL DEFAULT '-1',
+   `use_infocom_alert` tinyint NOT NULL DEFAULT '0',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_ticketunresolveds`;
 CREATE TABLE `glpi_plugin_additionalalerts_ticketunresolveds` (
    `id` int unsigned NOT NULL auto_increment,
    `entities_id` int unsigned NOT NULL default '0',
-   `delay_ticket_alert` int unsigned NOT NULL DEFAULT '-1',
+   `delay_ticket_alert` int unsigned NOT NULL DEFAULT '0',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
@@ -30,7 +30,7 @@ DROP TABLE IF EXISTS `glpi_plugin_additionalalerts_inkalerts`;
 CREATE TABLE `glpi_plugin_additionalalerts_inkalerts` (
    `id` int unsigned NOT NULL auto_increment,
    `entities_id` int unsigned NOT NULL default '0',
-   `use_ink_alert` tinyint NOT NULL DEFAULT '-1',
+   `use_ink_alert` tinyint NOT NULL DEFAULT '0',
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
